dd45fb194353aa4c8b18d093bbbab99934dd91f8
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2021-12-19 14:07:02 -0300

    feat: adds build function to get a file csv

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,5 +1,11 @@
 defmodule ReportsGenerator do
-  def hello do
-    :world
+  def build(filename) do
+    # Uma forma de retornamos apenas o valor, seja do resultado se a operação for
+    # bem sucedida, ou a mensagem do error.
+    case File.read("reports/#{filename}") do
+      {:ok, result } -> result
+      {:error, reason } -> reason
+      _ -> "caso nem dê sucesso, nem dê erro, vai cair aqui"
+    end
   end
 end
