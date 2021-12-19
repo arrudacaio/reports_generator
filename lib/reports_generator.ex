d49d31da3eabf42859f9ddf2cd942207415f1d25
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2021-12-19 17:23:11 -0300

    feat: adds build and parse line function to build a report

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -9,15 +9,26 @@ defmodule ReportsGenerator do
   #   end
   # end
 
-  def build(filename) do
-    "reports/#{filename}"
-    |> File.read()
-    |> handle_file
-  end
-
-  ## Vamos criar uma função privada para fazermos o pattern matching
+    ## Vamos criar uma função privada para fazermos o pattern matching
   # 1) Se der tudo certo, retorna o file_content
   # 2) Se der errado, retorna uma mensagem de erro
-  defp handle_file({:ok, file_content}), do: file_content
-  defp handle_file({:error, _reason}), do: "Error while open file!"
+  # defp handle_file({:ok, file_content}), do: file_content
+  # defp handle_file({:error, _reason}), do: "Error while open file!"
+
+  def build(filename) do
+    "reports/#{filename}"
+    |> File.stream!()
+    |> Enum.map(fn line -> parse_line(line) end)
+  end
+
+
+  defp parse_line(line) do
+    line
+    |> String.trim()
+    |> String.split(",")
+    |> List.update_at(2, &String.to_integer/1)
+    |> IO.inspect()
+  end
+
+
 end
