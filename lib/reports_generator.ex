8b41e0ff50c66ddcfa4d472a39810f6775fdd843
Caio Arruda <caio.arruda@ccc.ufcg.edu.br>
2021-12-19 14:45:00 -0300

    feat: adds build function to handle a file with pipe operator

--- lib/reports_generator.ex
+++ lib/reports_generator.ex
@@ -1,11 +1,23 @@
 defmodule ReportsGenerator do
+  # def build(filename) do
+  #   # Uma forma de retornamos apenas o valor, seja do resultado se a operação for
+  #   # bem sucedida, ou a mensagem do error.
+  #   case File.read("reports/#{filename}") do
+  #     {:ok, result } -> result
+  #     {:error, reason } -> reason
+  #     _ -> "caso nem dê sucesso, nem dê erro, vai cair aqui"
+  #   end
+  # end
+
   def build(filename) do
-    # Uma forma de retornamos apenas o valor, seja do resultado se a operação for
-    # bem sucedida, ou a mensagem do error.
-    case File.read("reports/#{filename}") do
-      {:ok, result } -> result
-      {:error, reason } -> reason
-      _ -> "caso nem dê sucesso, nem dê erro, vai cair aqui"
-    end
+    "reports/#{filename}"
+    |> File.read()
+    |> handle_file
   end
+
+  ## Vamos criar uma função privada para fazermos o pattern matching
+  # 1) Se der tudo certo, retorna o file_content
+  # 2) Se der errado, retorna uma mensagem de erro
+  defp handle_file({:ok, file_content}), do: file_content
+  defp handle_file({:error, _reason}), do: "Error while open file!"
 end
